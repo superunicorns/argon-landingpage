d8c2b19dc84d1c50376ebf5ee3daf1463b1d3bce
UnicornDad8 <unicorndad8@gmail.com>
2025-03-09 21:23:59 -0300

    Add hero background

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+
+function App() {
+  return (
+   <div className="relative w-full h-screen">
+    <section className="section h-full w-full py-4 lg:py-6 section-shaped pb-[100px]">
+      <div className="shape section-shaped shape-style-1 shape-default">
+        <span id="span-opacity-1"></span>
+        <span id="span-opacity-1"></span>
+        <span id="span-opacity-3"></span>
+        <span id="span-opacity-2"></span>
+        <span id="span-opacity-0"></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </section>
+   </div>
+  )
+}
+
+export default App;
