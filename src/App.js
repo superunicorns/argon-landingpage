2f1e8b145bb08fb56d9f71ef8f3b4f64af60e4e1
UnicornDad8 <unicorndad8@gmail.com>
2025-03-11 08:54:48 -0300

    Add navbar with transition from transparent to solid

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
+import { useState, createRef, useEffect } from "react";
+import Navbar, { Navbackground } from "./components/Navbar";
 import "./App.css";
 
+const headerRef = createRef();
+
 function App() {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const headerHeight = headerRef.current.clientHeight;
+    const range = 200;
+    const offset = headerHeight / 2;
+
+    const didScrollPage = e => {
+      let calc = 1 - (window.scrollY - offset + range) / range;
+
+      if (calc > 1) {
+        calc = 1;
+      } else if (calc < 0) {
+        calc = 0;
+      }
+
+      setOpacity(calc);
+    };
+
+    window.addEventListener("scroll", didScrollPage);
+
+    return () => {
+      window.removeEventListener("keydown", didScrollPage);
+    };
+  }, []);
+
   return (
-   <div className="relative w-full h-screen">
+   <div className="relative flex flex-col">
+    <Navbar />
+    <Navbackground opacity={opacity} />
+    <header ref={headerRef} className="relative w-full h-screen -mt-[40px]">
     <section className="section h-full w-full py-4 lg:py-6 section-shaped pb-[100px]">
       <div className="shape section-shaped shape-style-1 shape-default">
         <span id="span-opacity-1"></span>
@@ -16,6 +49,7 @@ function App() {
         <span></span>
       </div>
     </section>
+   </header>
    </div>
   )
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import { FaFacebookSquare } from "react-icons/fa";
+import { FaSquareTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import "../App.css";
+
+export const Navbackground = ({opacity}) => {
+  return <div id="nav-bg" className={opacity ? "opacity-0" : "opacity-100"} />;
+}
+
+const Navbar = () => {
+  return (
+    <nav  className="w-full px-4 py-4 h-[70px] fixed z-70 flex items-center">
+      <div  className="w-full max-w-5xl mx-auto flex justify-between items-center">
+      <img src="/assets/creativetim-white-slim.png" width={200} height="auto" alt="logo" />  
+      <div className="flex items-center gap-4">
+        <FaFacebookSquare size={25} color="#fff" />
+        <FaSquareTwitter size={25} color="#fff" />
+        <FaGithub size={25} color="#fff" />
+        <button type="button" className="mx-7 py-3 px-5 text-md text-[#7795f8] bg-white font-bold uppercase rounded-sm">
+          Try Demo
+        </button>
+      </div>
+      <div className="flex items-center gap-6">
+        <button type="button" className="py-3 px-5 text-md font-bold text-white uppercase">
+          Documentation
+        </button>
+        <button type="button" className="py-3 px-5 text-md font-bold text-white uppercase bg-[#11cdef] rounded-sm">
+          Upgrade to Pro
+        </button>
+      </div>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
