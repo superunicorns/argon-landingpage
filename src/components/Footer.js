94fbeb0fa5d2895503cf4f4f09f92a2bbacede10
UnicornDad8 <unicorndad8@gmail.com>
2025-03-18 11:25:38 -0300

    Make MobileScreen component responsive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="w-full relative">
       <div id="footer-background" className="absolute -z-10 -my-[200px] top-0 bottom-0 right-0 left-0" />
-      <div className="flex gap-10 mx-auto w-full items-center justify-center" >
+      <div className="flex gap-10 mx-auto w-full items-center justify-center mt-20" >
         <img src="/assets/microsoft-logo.png" alt="Microsoft" className="w-auto h-[80px]" />
         <div className="flex gap-6 flex-col lg:flex-row">
         <img src="/assets/typescript-logo.png" alt="TypeScript" width={80} height={80} className="rounded-full" />
--- src/components/MobileScreens.js
+++ src/components/MobileScreens.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 const MobileScreens = () => {
   return (
-    <div className="w-full justify-center items-center py-10 text-center">
-      <h3 className="px-4 my-20 text-3xl text-slate-700">A selection of screens</h3>
-      <div className="grid max-w-full gap-10 mx-auto md:max-w-xl lg:max-w-5xl grid-rows-4 md:grid-rows-2 lg:grid-rows-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        <div className="w-auto h-auto flex justify-center items-center">
-          <img src="/assets/mobile-screen-1.png" alt="mobile home screen" className="w-[300px] shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
+    <div className="w-full justify-center items-center py-10 px-4 text-center">
+      <h3 className="my-20 text-3xl text-slate-700">A selection of screens</h3>
+      <div className="grid w-[250px] sm:w-[400px] md:w-[600px] lg:w-[900px] place-content-center place-items-center mx-auto gap-6 sm:gap-10 grid-rows-4 md:grid-rows-2 lg:grid-rows-1 grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="mx-auto h-auto flex justify-center items-center">
+          <img src="/assets/mobile-screen-1.png" alt="mobile home screen" className="shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
         </div>
-        <div className="w-auto h-auto flex justify-center items-center">
-          <img src="/assets/mobile-screen-2.png" alt="mobile navigation screen" className="w-[300px] shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
+        <div className="h-auto flex justify-center items-center">
+          <img src="/assets/mobile-screen-2.png" alt="mobile navigation screen" className="shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
         </div>
-        <div className="w-auto h-auto flex justify-center items-center">
-          <img src="/assets/mobile-screen-3.png" alt="mobile profile screen" className="w-[300px] shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
+        <div className="h-auto flex justify-center items-center">
+          <img src="/assets/mobile-screen-3.png" alt="mobile profile screen" className="shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
         </div>
-        <div className="w-auto h-auto flex justify-center items-center">
-          <img src="/assets/mobile-screen-4.png" alt="mobile sign-up screen" className="w-[300px] shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
+        <div className="h-auto flex justify-center items-center">
+          <img src="/assets/mobile-screen-4.png" alt="mobile sign-up screen" className="shadow-xl shadow-slate-500/50 inset-shadow-sm rounded-xl" />
         </div>
       </div>
     </div>
